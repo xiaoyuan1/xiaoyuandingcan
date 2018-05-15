bb4a49589610085cc0f20956b408e0f92c4fddcc
zexing <37724540+zexing@users.noreply.github.com>
2018-05-15 16:24:26 +0800

    主函数

--- hero/hero-service/src/test/java/edu/zut/cs/software/hero/admin/service/AdminEntityGenerator.java
+++ hero/hero-service/src/test/java/edu/zut/cs/software/hero/admin/service/AdminEntityGenerator.java
@@ -7,18 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.zut.cs.software.hero.admin.domain.Foot;
 import edu.zut.cs.software.hero.admin.domain.Group;
-import edu.zut.cs.software.hero.admin.domain.User;
+import edu.zut.cs.software.hero.admin.domain.Push;
 import edu.zut.cs.software.hero.base.service.GenericGenerator;
 
 public class AdminEntityGenerator extends GenericGenerator {
+
 	@Autowired
 	GroupManager groupManager;
 
 	@Autowired
-	UserManager userManager;
-	
-	@Autowired
 	FootManager footManager;
+
+	@Autowired
+	PushManager pushManager;
+
 
 	@Test
 	public void gen_group() {
@@ -44,8 +46,9 @@ public class AdminEntityGenerator extends GenericGenerator {
 			this.footManager.save(u);
 		}
 	}
+
 	@Test
-	public void gen_group1() {
+	public void gen_group10() {
 		for (int i = 0; i < 10; i++) {
 			Group g = new Group();
 			g.setName("group_" + i);
@@ -55,17 +58,20 @@ public class AdminEntityGenerator extends GenericGenerator {
 				group.setName("group_" + i + "_" + j);
 				group.setParent(g);
 				g = this.groupManager.save(group);
-				this.gen_user(g);
+				this.gen_push(g);
 			}
 		}
 	}
-	public void gen_user(Group g) {
+	public void gen_push(Group g) {
 		for (int i = 0; i < 10; i++) {
-			User u = new User();
-			u.setUsername("username_" + i);
-			u.setPassword("password_" + i);
-			u.setGroup(g);
-			this.userManager.save(u);
+			Push p= new Push();
+			p.setMessage("pushname_" + i);
+			//u.setPassword("password_" + i);
+			p.setGroup(g);
+			this.pushManager.save(p);
 		}
 	}
+
 }
+
+
