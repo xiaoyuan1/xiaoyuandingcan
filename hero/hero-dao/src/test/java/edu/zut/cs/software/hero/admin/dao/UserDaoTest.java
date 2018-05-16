051daa560d4d60cff3daa1dd94b648f06f893351
995114879 <37178607+995114879@users.noreply.github.com>
2018-05-16 18:46:01 +0800

    更新userdaotest

--- hero/hero-dao/src/test/java/edu/zut/cs/software/hero/admin/dao/UserDaoTest.java
+++ hero/hero-dao/src/test/java/edu/zut/cs/software/hero/admin/dao/UserDaoTest.java
@@ -4,19 +4,29 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import edu.zut.cs.software.hero.admin.domain.User;
+import edu.zut.cs.software.hero.base.dao.GenericDaoTestCase;
 
 
 
-public class UserDaoTest {
+public class UserDaoTest extends GenericDaoTestCase<Long, User, UserDao>  {
 	@Autowired
 	UserDao userDao;
 	@Test
 	public void test() {
-		List<User> result = this.userDao.findAll();
+		User u = new User();
+		u.setUsername("test_user");
+		User test_user = this.userDao.save(u);
+		Long user_id = test_user.getId();
+		User result = this.userDao.getOne(user_id);
+		assertEquals(test_user, result);
+		List<User> all = this.userDao.findAll();
 	}
 
 }
