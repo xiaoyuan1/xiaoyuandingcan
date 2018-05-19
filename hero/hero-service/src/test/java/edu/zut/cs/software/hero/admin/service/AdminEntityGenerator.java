53cc2ee61a91c75edb65945dd838949dd340ad57
zexing <37724540+zexing@users.noreply.github.com>
2018-05-19 16:50:20 +0800

    删除无效代码段

--- hero/hero-service/src/test/java/edu/zut/cs/software/hero/admin/service/AdminEntityGenerator.java
+++ hero/hero-service/src/test/java/edu/zut/cs/software/hero/admin/service/AdminEntityGenerator.java
@@ -5,88 +5,25 @@ package edu.zut.cs.software.hero.admin.service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import edu.zut.cs.software.hero.Foot.domain.Foot;
 import edu.zut.cs.software.hero.admin.domain.Depot;
 import edu.zut.cs.software.hero.admin.domain.Group;
 import edu.zut.cs.software.hero.admin.domain.Order;
 import edu.zut.cs.software.hero.base.service.GenericGenerator;
-import edu.zut.cs.software.hero.foot.service.FootManager;
-import edu.zut.cs.software.hero.push.domain.Push;
-import edu.zut.cs.software.hero.push.service.PushManager;
-import edu.zut.cs.software.hero.user.domain.User;
-import edu.zut.cs.software.hero.user.service.UserManager;
 
 public class AdminEntityGenerator extends GenericGenerator {
 
 	@Autowired
 	GroupManager groupManager;
 
-	@Autowired
-	FootManager footManager;
-
-	@Autowired
-	PushManager pushManager;
 
 	@Autowired
 	DepotManager depotManager;
 	
 	@Autowired
-	UserManager userManager;
-   
-	@Autowired
 	OrderManager orderManager;
 
-	@Test
-	public void gen_group() {
-		for (int i = 0; i < 10; i++) {
-			Group g = new Group();
-			g.setName("group_" + i);
-			this.groupManager.save(g);
-			for (int j = 0; j < 10; j++) {
-				Group group = new Group();
-				group.setName("group_" + i + "_" + j);
-				group.setParent(g);
-				g = this.groupManager.save(group);
-				this.gen_foot(g);
-			}
-		}
-	}
-	public void gen_foot(Group g) {
-		for (int i = 0; i < 10; i++) {
-			Foot u = new Foot();
-			u.setProduct_name("footname_" + i);
-			//u.setPassword("password_" + i);
-			u.setGroup(g);
-			this.footManager.save(u);
-		}
-	}
-
-	@Test
-	public void gen_group10() {
-		for (int i = 0; i < 10; i++) {
-			Group g = new Group();
-			g.setName("group_" + i);
-			this.groupManager.save(g);
-			for (int j = 0; j < 10; j++) {
-				Group group = new Group();
-				group.setName("group_" + i + "_" + j);
-				group.setParent(g);
-				g = this.groupManager.save(group);
-				this.gen_push(g);
-			}
-		}
-	}
-	public void gen_push(Group g) {
-		for (int i = 0; i < 10; i++) {
-			Push p= new Push();
-			p.setMessage("pushname_" + i);
-			//u.setPassword("password_" + i);
-			p.setGroup(g);
-			this.pushManager.save(p);
-		}
-	}
-	@Test
 	
+	@Test
 	public void gen_group11() {
 		for (int i = 0; i < 10; i++) {
 			Group g = new Group();
@@ -105,36 +42,11 @@ public class AdminEntityGenerator extends GenericGenerator {
 		for (int i = 0; i < 10; i++) {
 			Depot d = new Depot();
 			d.setRaw("name" + i);
-			//u.setPassword("password_" + i);
 			d.setGroup(g);
 			this.depotManager.save(d);
 		}
 	}
-	@Test
-	public void gen_group12() {
-		for (int i = 0; i < 10; i++) {
-			Group g = new Group();
-			g.setName("group_" + i);
-			this.groupManager.save(g);
-			for (int j = 0; j < 10; j++) {
-				Group group = new Group();
-				group.setName("group_" + i + "_" + j);
-				group.setParent(g);
-				g = this.groupManager.save(group);
-				this.gen_user(g);
-			}
-		}
-	}
-
-	public void gen_user(Group g) {
-		for (int i = 0; i < 10; i++) {
-			User u = new User();
-			u.setUsername("username_" + i);
-			u.setPassword("password_" + i);
-			u.setGroup(g);
-			this.userManager.save(u);
-		}
-	}
+	
 
    @Test
    public void gen_group18() {
--- hero/hero-service/src/test/java/edu/zut/cs/software/hero/push/service/PushManagerTest.java
+++ hero/hero-service/src/test/java/edu/zut/cs/software/hero/push/service/PushManagerTest.java
@@ -1,11 +1,8 @@
 package edu.zut.cs.software.hero.push.service;
 
-import static org.junit.Assert.*;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import edu.zut.cs.software.hero.base.service.GenericGenerator;
 import edu.zut.cs.software.hero.push.domain.Push;
 
@@ -13,20 +10,18 @@ import edu.zut.cs.software.hero.push.domain.Push;
 public class PushManagerTest  extends GenericGenerator{
 	@Autowired
 	PushManager pushManager;
-	@Test
-	public void testFindAll() {
-		List<Push> all = this.pushManager.findAll();
-		assertEquals(all.size(), 100);
-	}
 
 	@Test
-	public void testFindbyPushname() {
-		String pushname = "world";
-		Push expected_push = new Push();
-		// expected_user.setUsername(username);
-		Push push = this.pushManager.findbyPushname(pushname);
-		// assertEquals(user.getUsername(), expected_user.getUsername());
-		assertEquals(push, expected_push);
+	public void gen_group10() {
+		for (int i = 0; i < 10; i++) {
+			Push g = new Push();
+			g.setMessage("group_" + i);
+			this.pushManager.save(g);
+			for (int j = 0; j < 10; j++) {
+				Push group = new Push();
+				group.setMessage("group_" + i + "_" + j);
+				g = this.pushManager.save(group);
+			}
+		}
 	}
-
 }
