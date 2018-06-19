71257d7cea700ab791f5df10e753408e23943841
995114879 <37178607+995114879@users.noreply.github.com>
2018-06-19 13:42:08 +0800

    删除

--- hero/hero-dao/src/main/java/edu/zut/cs/software/hero/Comment/dao/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.cs.software.hero.comment.dao;
-
-import edu.zut.cs.software.hero.comment.domain.Comment;
-import edu.zut.cs.software.hero.base.dao.GenericDao;
-
-
-public interface CommentDao extends GenericDao<Comment, Long>{
-
-}
